5a85768b567e7ed2d4daf544443da76d9ef510b6
ArkPac97 <arekpachuta97@gmail.com>
2025-04-14 22:32:27 +0200

    Zadanie 5: API z regułą decyzyjną  /api/v1.0/predict

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -17,6 +17,25 @@ def hello():
         return f"Hello {name}!"
     else:
         return "Hello!"
+
+@app.route('/api/v1.0/predict')
+def predict():
+    try:
+        num1 = float(request.args.get('num1', 0))
+        num2 = float(request.args.get('num2', 0))
+    except (TypeError, ValueError):
+        return jsonify({"error": "Invalid input"}), 400
+
+    total = num1 + num2
+    prediction = 1 if total > 5.8 else 0
+
+    return jsonify({
+        "prediction": prediction,
+        "features": {
+            "num1": num1,
+            "num2": num2
+        }
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
